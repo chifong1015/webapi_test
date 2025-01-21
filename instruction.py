0444c1f0e9527bc5389a081160f4ecae71d18376
chifong <zxc101589@gmail.com>
2025-01-21 17:22:36 +0800

    測試class

---
+++ instruction.py
@@ -0,0 +1,13 @@
+import subprocess 
+
+class instruction():
+    def Tojunit(self,name):  
+        Tj = subprocess.run("pytest .\\"+name+" --junitxml junit-report.xml",shell=True, text=True, capture_output=True) 
+        return Tj.stdout ,Tj.stderr
+    
+    def trcli(self,username,passsword, testrailURL, project,file, title, log = False): 
+        if log:
+            tr = subprocess.run("trcli -u "+username+" -p "+passsword+" -h "+testrailURL+" --project "+project+" -v  parse_junit -f "+file+" --title "+title,shell=True, text=True, capture_output=True) 
+        else:
+            tr = subprocess.run("trcli -u "+username+" -p "+passsword+" -h "+testrailURL+" --project "+project+" parse_junit -f "+file+" --title "+title,shell=True, text=True, capture_output=True) 
+        return tr.stdout ,tr.stderr
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pytest
+from instruction import instruction
 
 def test_sum_two_numbers():
     assert 1 + 1 == 2
@@ -10,3 +11,6 @@ def test_sum_two_decimals(record_property):
 @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6)])
 def test_sum_multiple_numbers(test_input, expected):
     assert eval(test_input) == expected
+
+instruction().Tojunit("test.py")
+instruction().trcli("john.jian@insynerger.com","Zxc101589.","https://chifong0123456.testrail.io/","test",".\\junit-report.xml","123")
