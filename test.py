836740ffe1e53c3bcbc3656d2f6d1bc2e9462417
chifong <zxc101589@gmail.com>
2025-01-23 09:28:07 +0800

    測試全部

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ add = AutoAdd()
 section = {
     "name": "test",
 }
-add.Addsection("test2",section)
+add.Addsection("test",section)
 case = [
     {
      "title": "test_sum_two_decimals",
@@ -14,7 +14,8 @@ case = [
      "custom_preconds": "123",
      "custom_steps": "My test steps",
      "custom_expected": "My expected final results"
-    },
+    }
+    ,
     {
      "title": "test_sum_two_decimals",
      "refs": "123",
@@ -38,16 +39,18 @@ case = [
      "custom_preconds": "123",
      "custom_steps": "My test steps",
      "custom_expected": "My expected final results"
-    },
+    }
 ]
-add.Addcase('test2','test',case)
-def test_sum_two_numbers():
-    assert 1 + 1 == 2
+for i in case:
+    print(add.Addcase('test','test',i))
 
-def test_sum_two_decimals(record_property):
-    record_property("testrail_attachment", "sample_reports/testrail.jpg")
-    assert 0.8 + 0.3 == 1.2
+# def test_sum_two_numbers():
+#     assert 1 + 1 == 2
 
-@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6)])
-def test_sum_multiple_numbers(test_input, expected):
-    assert eval(test_input) == expected
+# def test_sum_two_decimals(record_property):
+#     record_property("testrail_attachment", "sample_reports/testrail.jpg")
+#     assert 0.8 + 0.3 == 1.2
+
+# @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6)])
+# def test_sum_multiple_numbers(test_input, expected):
+#     assert eval(test_input) == expected
