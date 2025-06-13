3366a2053eeddb8870eeed9fde5469155c70d53d
chifong <zxc101589@gmail.com>
2025-06-13 11:17:17 +0800

    test12346

--- file.py
+++ file.py
@@ -26,10 +26,16 @@ class AutoAdd():
         id = self.GetPID("get_projects",Project)
         get = requests.get(self.URL+"get_sections/"+str(id),headers=self.headers).json()
         for i in get['sections']:
-            if i['name'] == addinsection: sID = i['id']
+            if i['name'] == addinsection: sID = i['id']       
         reponses = requests.post(self.URL+'add_case/'+str(sID),json=data,headers=self.headers).json()
         return reponses
-
+auth_header = base64.b64encode(f"{'john.jian@insynerger.com'}:{'I3JZQgjhRyZrQVMW21sT-u47Nw36YQNj0O0Od6gfR'}".encode()).decode()
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Basic {auth_header}"
+}
+get = requests.get("https://chifong0123456.testrail.io/index.php?/api/v2/get_cases/4",headers=headers).json()
+print(get)
 # data = {
 #     "name": "New Section Name",
 #     "description": "Optional description for this section",    
